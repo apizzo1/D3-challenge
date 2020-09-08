6e5fb8f7121f24e516ca0070013c5217076ea49d
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-09-07 22:38:49 -0400

    updating circle attributes

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -2,7 +2,7 @@
 
 
 // Define SVG area dimensions
-var svgWidth = 960;
+var svgWidth = 800;
 var svgHeight = 660;
 
 // Define the chart's margins as an object
@@ -38,11 +38,12 @@ d3.csv("data/data.csv")
     healthData.forEach(function(d) {
         d.healthcare = +d.healthcare;
         d.obesity = +d.obesity;
+        d.poverty = +d.poverty;
     });
 
-    //  Configure a linear scale for the horizontal axis 
+    // Create a linear scale for the horizontal axis 
     var xscale = d3.scaleLinear()
-        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .domain([3, d3.max(healthData, d => d.healthcare)])
         .range([0, chartWidth]);
 
     // Create a linear scale for the vertical axis.
@@ -66,43 +67,44 @@ d3.csv("data/data.csv")
     
     // append initial circles
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(healthData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xscale(d.healthcare))
-    .attr("cy", d => yscale(d.obesity))
-    .attr("r", 10)
-    .attr("fill", "blue")
-    .attr("opacity", ".5");
+      .data(healthData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xscale(d.healthcare))
+      .attr("cy", d => yscale(d.obesity))
+      .attr("r", 10)
+      .attr("fill", "blue")
+      .attr("opacity", ".4");
 
     // add text to circles 
-    textLabels = chartGroup.selectAll("text")
+    // source: https://stackoverflow.com/questions/49882951/labels-for-circles-not-showing-up-in-d3-data-visualization
+    textLabels = chartGroup.append('g')
+      .selectAll("text")
       .data(healthData)
       .enter()
       .append("text")
       .attr("x", d => xscale(d.healthcare-0.2))
-      .attr("y", d => yscale(d.obesity-0.15))
+      .attr("y", d => yscale(d.obesity-0.13))
       .text( d => d.abbr)
       .attr("font-family", "sans-serif")
       .attr("font-size", "10px")
       .attr("fill", "white");
 
     
-      
-
-    // append y axis
+    // append y axis label
     chartGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - (chartMargin.left))
     .attr("x", 0 - (chartHeight / 2))
     .attr("dy", "1em")
     .classed("axis-text", true)
-    .text("Obesity %");
+    .text("Obesity (%)");
 
+    // append x axis label
     chartGroup.append("text")
     .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top })`)
     .attr("class", "axisText")
-    .text("Healthcare %");
+    .text("Healthcare (%)");
 
 
 })
