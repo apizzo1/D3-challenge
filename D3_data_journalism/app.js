abace5abde6816d34122117cd12cf71e43be6373
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-09-07 21:27:42 -0400

    adding data

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -34,34 +34,46 @@ d3.csv("data/data.csv")
 .then((healthData) => {
     console.log(healthData);
 
-// Cast the healthcare value to a number for all data
-healthData.forEach(function(d) {
-    d.healthcare = +d.healthcare;
-    d.obesity = +d.obesity;
-  });
+    // Cast the healthcare value to a number for all data
+    healthData.forEach(function(d) {
+        d.healthcare = +d.healthcare;
+        d.obesity = +d.obesity;
+    });
 
-  //  Configure a linear scale for the horizontal axis 
-var xscale = d3.scaleLinear()
-    .domain(healthData.map(d => d.healthcare))
-    .range([0, chartWidth]);
+    //  Configure a linear scale for the horizontal axis 
+    var xscale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .range([0, chartWidth]);
 
-// Create a linear scale for the vertical axis.
-var yscale = d3.scaleLinear()
-    .domain([0, d3.max(healthData, d => d.obesity)])
-    .range([chartHeight, 0]);
+    // Create a linear scale for the vertical axis.
+    var yscale = d3.scaleLinear()
+        .domain([20, d3.max(healthData, d => d.obesity)])
+        .range([chartHeight, 0]);
 
-var bottomAxis = d3.axisBottom(xscale);
-var leftAxis = d3.axisLeft(yscale);
+    var bottomAxis = d3.axisBottom(xscale);
+    var leftAxis = d3.axisLeft(yscale);
+    
+    //   // Append two SVG group elements to the chartGroup area,
+    //   // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+        .call(leftAxis);
   
-  //   // Append two SVG group elements to the chartGroup area,
-  //   // and create the bottom and left axes inside of them
-chartGroup.append("g")
-    .call(leftAxis);
-  
-chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-  
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+    
+    //   // Create one SVG circle per piece of healthdata
+    
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xscale(d.healthcare))
+    .attr("cy", d => yscale(d.obesity))
+    .attr("r", 10)
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
 
 
 
